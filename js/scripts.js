4a7df26a6587488ee8857defb39862d7c75497e0
Malcolm Womble <malcolmaw@icloud.com>
2019-10-25 14:08:45 -0700

    add .hide feature when someone takes the test again with different answers

--- js/scripts.js
+++ js/scripts.js
@@ -3,27 +3,33 @@ $(document).ready(function() {
     event.preventDefault();
 
     var question1Score = parseInt($("input:radio[name=choice1]:checked").val());
-    alert(question1Score);
+
     var question2Score = parseInt($("input:radio[name=choice2]:checked").val());
-    alert(question2Score);
+
     var question3Score = parseInt($("input:radio[name=choice3]:checked").val());
-    alert(question3Score);
+
     var question4Score = parseInt($("input:radio[name=choice4]:checked").val());
-    alert(question4Score);
+
     var question5Score = parseInt($("input:radio[name=choice5]:checked").val());
-    alert(question5Score);
+
     var totalScore =  parseInt(question1Score) + parseInt(question2Score) + parseInt(question3Score) + parseInt(question4Score) + parseInt(question5Score);
     alert(totalScore)
 
     if (totalScore >= 11) {
+      $("#python").hide();
+      $("#cSharp").hide();
       $("#ruby").show();
     }
 
     else if (totalScore <= 5) {
+      $("#cSharp").hide();
+      $("#ruby").hide();
       $("#python").show();
     }
 
     else if (totalScore < 11) {
+      $("#ruby").hide();
+      $("#python").hide();
       $("#cSharp").show();
     }
   });
