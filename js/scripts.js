aee163499d21e62d0aee971206527810aac267da
Malcolm Womble <malcolmaw@icloud.com>
2019-10-25 13:49:08 -0700

    debugged if/else statements, form class, form id, div id's linked to .show

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,30 @@
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
+    event.preventDefault();
 
-    var question1Score = parseInt($("input:radio[name=choice1]:checked").val())
-    alert(question1Score)
-    var question2Score = parseInt($("input:radio[name=choice2]:checked").val())
-    alert(question2Score)
-    var question3Score = parseInt($("input:radio[name=choice3]:checked").val())
-    alert(question3Score)
-    var question4Score = parseInt($("input:radio[name=choice4]:checked").val())
-    alert(question4Score)
-    var question5Score = parseInt($("input:radio[name=choice5]:checked").val())
-    alert(question5Score)
-    var totalScore =  parseInt(question1Score) + parseInt(question2Score) + parseInt(question3Score) + parseInt(question4Score) + parseInt(question5Score)
+    var question1Score = parseInt($("input:radio[name=choice1]:checked").val());
+    alert(question1Score);
+    var question2Score = parseInt($("input:radio[name=choice2]:checked").val());
+    alert(question2Score);
+    var question3Score = parseInt($("input:radio[name=choice3]:checked").val());
+    alert(question3Score);
+    var question4Score = parseInt($("input:radio[name=choice4]:checked").val());
+    alert(question4Score);
+    var question5Score = parseInt($("input:radio[name=choice5]:checked").val());
+    alert(question5Score);
+    var totalScore =  parseInt(question1Score) + parseInt(question2Score) + parseInt(question3Score) + parseInt(question4Score) + parseInt(question5Score);
+    alert(totalScore)
 
     if (totalScore >= 11) {
-      $("ruby").show();
+      $("#ruby").show();
     }
 
     else if (totalScore <= 5) {
-      $("python").show();
+      $("#python").show();
     }
 
     else if (totalScore < 11) {
-      $("cSharp").show();
+      $("#cSharp").show();
     }
-    event.preventDefault();
   });
 });
